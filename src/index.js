8bfafe8c22c92a219113a36120a45b30b58e8c25
osi2709 <a.osipova2709@gmail.com>
2022-09-05 23:43:15 +0300

    update img

--- src/index.js
+++ src/index.js
@@ -35,6 +35,14 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  let iconElement = document.querySelector("#icon");
 }
 
 function search(event) {
